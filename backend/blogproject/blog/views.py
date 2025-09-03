7f33f95d967fa0ae0279fd803a35ce7e0666fcb2
B.Nandhakumar <nandhakumarid023@gmail.com>
2025-09-03 13:10:05 +0530

    views.py

--- backend/blogproject/blog/views.py
+++ backend/blogproject/blog/views.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponse
 from rest_framework import generics
 from .models import Blog, Category
 from .serializers import BlogSerializer, CategorySerializer
 
+# Your previous API views
 class BlogList(generics.ListAPIView):
     queryset = Blog.objects.all().order_by("-created_at")
     serializer_class = BlogSerializer
@@ -13,3 +15,7 @@ class BlogDetail(generics.RetrieveAPIView):
 class CategoryList(generics.ListAPIView):
     queryset = Category.objects.all().order_by("name")
     serializer_class = CategorySerializer
+
+# Add this at the end
+def home(request):
+    return HttpResponse("Welcome to My Blog API!")
