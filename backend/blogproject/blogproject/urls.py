23d97ac65807664119eea58e678114f71c321f6c
B.Nandhakumar <nandhakumarid023@gmail.com>
2025-09-03 13:06:00 +0530

    urls.py

--- backend/blogproject/blogproject/urls.py
+++ backend/blogproject/blogproject/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from blog.views import BlogList, BlogDetail, CategoryList
+from blog.views import BlogList, BlogDetail, CategoryList, home
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", home, name="home"),  # ✅ root path
     path("api/blogs/", BlogList.as_view(), name="blog-list"),
     path("api/blogs/<int:pk>/", BlogDetail.as_view(), name="blog-detail"),
     path("api/categories/", CategoryList.as_view(), name="category-list"),
